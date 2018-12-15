0a1559c56bbf0ab13ad6a8493ff8418f49277e8e
Ami <amirouche@gmail.com>
2018-12-15 20:52:07 +0100

    .

--- quienessomos.php
+++ quienessomos.php
@@ -15,10 +15,34 @@ require_once 'application/parts/frontend/header.php';
                     <h4 class="card-title"><?= $title ?></h4>
                     </div>
                 <div class="card-body">
-                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet deserunt dignissimos distinctio fugit illo ipsum, minus nemo obcaecati optio quam qui quidem quis saepe, similique suscipit tempore, vel. Ipsam, numquam.</p>
-										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque debitis minima nam nesciunt, nostrum quia rerum ut. Accusamus aut eveniet harum inventore sequi suscipit veritatis? Omnis optio placeat qui sapiente?</p>
+										<p><strong>Movilelx</strong> es una empresa que pone al servicio de sus clientes los mejores productos</p>
 
-                </div>
+										<p> de telefonía y electrónica a los mejores precios del mercado.
+
+										<p>Podéis encontrar en nuestra tienda online, desde los últimos móviles salidos</p>
+
+										<p> al mercado, hasta los mejores productos de informática para cubrir todas vuestras</p>
+
+										<p> necesidades a los mejores precios, <strong>Movilelx</strong> no es solo una tienda de electrónica.</p>
+
+										<p>Somos los mejores proveedores de todo lo que necesitas para estar al día en informática, telefonía y te </p>
+
+										<p>ofrecemos precios sin competencia y productos de la mejor calidad.</p>
+
+										<p>Todo lo relacionado con electrónica está al alcance de tu mano en </p>
+
+										<p><strong>Movilelx </strong> con precio sin competencia y con la fiabilidad</p>
+
+										<p> de una empresa líder en el sector que os acerca a casa todo lo que deseáis de las últimas</p>
+
+										<p>tendencias en tecnología. Un clic te separa hacer tus deseos realidad, con la mayor facilidad del mercado</p>
+
+										<p>en compra y pago de nuestros productos. No hay competencia, </p>
+
+										<p>porque nadie te da lo que nosotros te damos. Calidad, precio y facilidad de compra.</p>
+
+
+								</div>
             </div>
         </div>
     </div>
