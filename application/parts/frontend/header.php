1bec1d16718b6c7e901dd739344f42dc5357a296
Ami <amirouche@gmail.com>
2019-01-27 10:36:30 +0100

    .

--- application/parts/frontend/header.php
+++ application/parts/frontend/header.php
@@ -9,6 +9,7 @@
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="assets/frontend/css/bootstrap.min.css">
 		<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+		
 		<link rel="stylesheet" href="assets/frontend/css/style.css">
 		<title>MovilElx| <?= (isset($title) ? $title: "no title ")?></title>
 </head>
